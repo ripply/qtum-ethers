b46b424d1d68c476228067fb45fab55d1e9fe6ba
Chase Ripplinger <chase@ripply.io>
2022-03-14 10:24:00 -0500

    npm run build

--- build/main/index.js
+++ build/main/index.js
@@ -12,7 +12,9 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
 Object.defineProperty(exports, "__esModule", { value: true });
 __exportStar(require("./lib/QtumProvider"), exports);
 __exportStar(require("./lib/QtumFunctionProvider"), exports);
+__exportStar(require("./lib/QtumTestProvider"), exports);
 __exportStar(require("./lib/QtumContractFactory"), exports);
 __exportStar(require("./lib/QtumWallet"), exports);
+__exportStar(require("./lib/deployContract"), exports);
 __exportStar(require("./lib/helpers/utils"), exports);
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7O0FBQUEscURBQW1DO0FBQ25DLDZEQUEyQztBQUMzQyw0REFBMEM7QUFDMUMsbURBQWlDO0FBQ2pDLHNEQUFvQyJ9
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7O0FBQUEscURBQW1DO0FBQ25DLDZEQUEyQztBQUMzQyx5REFBdUM7QUFDdkMsNERBQTBDO0FBQzFDLG1EQUFpQztBQUNqQyx1REFBcUM7QUFDckMsc0RBQW9DIn0=
--- build/module/index.js
+++ build/module/index.js
@@ -1,6 +1,8 @@
 export * from "./lib/QtumProvider";
 export * from "./lib/QtumFunctionProvider";
+export * from "./lib/QtumTestProvider";
 export * from "./lib/QtumContractFactory";
 export * from "./lib/QtumWallet";
+export * from "./lib/deployContract";
 export * from "./lib/helpers/utils";
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsY0FBYyxvQkFBb0IsQ0FBQztBQUNuQyxjQUFjLDRCQUE0QixDQUFDO0FBQzNDLGNBQWMsMkJBQTJCLENBQUM7QUFDMUMsY0FBYyxrQkFBa0IsQ0FBQztBQUNqQyxjQUFjLHFCQUFxQixDQUFDIn0=
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi8uLi9zcmMvaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsY0FBYyxvQkFBb0IsQ0FBQztBQUNuQyxjQUFjLDRCQUE0QixDQUFDO0FBQzNDLGNBQWMsd0JBQXdCLENBQUM7QUFDdkMsY0FBYywyQkFBMkIsQ0FBQztBQUMxQyxjQUFjLGtCQUFrQixDQUFDO0FBQ2pDLGNBQWMsc0JBQXNCLENBQUM7QUFDckMsY0FBYyxxQkFBcUIsQ0FBQyJ9
